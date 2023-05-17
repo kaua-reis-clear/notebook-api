389be1f369f330f0c04f159fe5a7a0211d3e3f92
kaua-reis-clear <kaua.reis.clear@gmail.com>
2023-05-17 00:00:10 -0300

    feat: changed render as json method  to associate with other table

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,11 +1,19 @@
 class Contact < ApplicationRecord
   belongs_to :kind
-  
+
   def author
     "Kauã Reis"
   end
 
+  def kind_description
+    self.kind.description
+  end
+
   def as_json(options={})
-    super(methods: :author, root: true)
+    super(
+      root: true,
+      methods: [:kind_description, :author],
+      include: { kind: { only: :description } }
+    )
   end
 end
