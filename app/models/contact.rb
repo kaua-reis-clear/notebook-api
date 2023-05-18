396fedc2adbf5492b0eb49e55bbac41c52f3aa46
kaua-reis-clear <kaua.reis.clear@gmail.com>
2023-05-18 19:18:31 -0300

    feat: added model phone

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,24 +1,34 @@
 class Contact < ApplicationRecord
+  # Associations
   belongs_to :kind #, optional: true
+  has_many :phones
+
+  def as_json(options={})
+    h = super(options)
+    h[:birthdate] = (I18n.l(self.birthdate) unless self.birthdate.blank?)
+    h
+  end
 
   # def birthdate_br
   #   I18n.l(self.birthdate) unless self.birthdate.blank?
   # end
 
-  def to_br
-    {
-      name: self.name,
-      email: self.email,
-      birthdate: (I18n.l(self.birthdate) unless self.birthdate.blank?)
-    }
-  end
+  # def to_br
+  #   {
+  #     name: self.name,
+  #     email: self.email,
+  #     birthdate: (I18n.l(self.birthdate) unless self.birthdate.blank?)
+  #   }
+  # end
 
   # def author
-  #   "Kauã Reis"
+  #   "Jackson Pires"
   # end
+
   # def kind_description
   #   self.kind.description
   # end
+
   # def as_json(options={})
   #   super(
   #     root: true,
