a1bac765346469ff22b6d91662bf956b59d804ed
kaua-reis-clear <kaua.reis.clear@gmail.com>
2023-05-17 23:28:43 -0300

    feat: added i18n for date

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,27 +1,37 @@
 class Contact < ApplicationRecord
   belongs_to :kind #, optional: true
 
+  # def birthdate_br
+  #   I18n.l(self.birthdate) unless self.birthdate.blank?
+  # end
+
+  def to_br
+    {
+      name: self.name,
+      email: self.email,
+      birthdate: (I18n.l(self.birthdate) unless self.birthdate.blank?)
+    }
+  end
+
   # def author
   #   "Kauã Reis"
   # end
-
   # def kind_description
   #   self.kind.description
   # end
-
   # def as_json(options={})
   #   super(
   #     root: true,
   #     methods: [:kind_description, :author],
-  #     include: { kind: { only: :description } }
+  #     include: { kind: { only: :description }}
   #   )
   # end
 
-  def hello
-    I18n.t('hello')
-  end
+  # def hello
+  #   I18n.t('hello')
+  # end
 
-  def i18n
-    I18n.default_locale
-  end
+  # def i18n
+  #   I18n.default_locale
+  # end
 end
