ff39101525094c727ed8bd3207c5195db6176efb
kaua-reis-clear <kaua.reis.clear@gmail.com>
2023-05-22 23:04:32 -0300

    feat: using json adapter to change json response

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -1,3 +1,14 @@
 class ContactSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :name, :email, :birthdate
+
+  belongs_to :kind, optional: true
+  has_many :phones
+  has_one :address
+
+  def as_json(*args)
+    h = super(*args)
+    # pt-BR ---> h[:birthdate] = (I18n.l(object.birthdate) unless object.birthdate.blank?)
+    h[:birthdate] = object.birthdate.to_time.iso8601 unless object.birthdate.blank?
+    h
+  end
 end
