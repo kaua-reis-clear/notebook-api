9e3ca695790e2c1650610d59b1454bc21c2b0b53
kaua-reis-clear <kaua.reis.clear@gmail.com>
2023-05-23 22:08:16 -0300

    feat: added extra meta info on response

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -1,11 +1,19 @@
 class ContactSerializer < ActiveModel::Serializer
-  attributes :id, :name, :email, :birthdate
+  attributes :id, :name, :email, :birthdate #, :author
 
-  belongs_to :kind, optional: true
+  belongs_to :kind
   has_many :phones
   has_one :address
 
-  def as_json(*args)
+  # def author
+  #   "Kauã Reis"
+  # end
+
+  meta do
+    { author: "Kauã Reis" }
+  end
+
+  def attributes(*args)
     h = super(*args)
     # pt-BR ---> h[:birthdate] = (I18n.l(object.birthdate) unless object.birthdate.blank?)
     h[:birthdate] = object.birthdate.to_time.iso8601 unless object.birthdate.blank?
