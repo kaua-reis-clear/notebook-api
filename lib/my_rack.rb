fbbed844ab479b2580ff941348ca902d29c92ba2
kaua-reis-clear <kaua.reis.clear@gmail.com>
2023-05-29 22:09:03 -0300

    feat: created rack middleware

--- lib/my_rack.rb
+++ lib/my_rack.rb
@@ -1,7 +1,17 @@
-require 'rack'
+class AppName
+  def initialize(app, app_name)
+    @app = app
+    @app_name = app_name
+  end
 
-app = Proc.new do |env|
-  ['200', {'Content-Type' => 'text/html'}, ["#{env}"]]
+  def call(env)
+    puts ">>>>> #{env}"
+    if env["ORIGINAL_FULLPATH"] == "/"
+      ['200', {'Content-Type' => 'text/html'}, ["Teste!"]]
+    else
+      status, headers, response = @app.call(env)
+      headers.merge!({'X-App-Name' => "#{@app_name}"})
+      [status, headers, [response.body]]
+    end
+  end
 end
-
-Rack::Handler::WEBrick.run(app, Port: 3000, Host: '0.0.0.0')
